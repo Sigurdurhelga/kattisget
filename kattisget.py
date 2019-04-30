7382d2f91b7b36214a15a2a30e773e03f9556464
Sigurður Helgason <sigurdurhel15@ru.is>
2019-04-30 17:59:47 +0000

    added the script

---
+++ kattisget.py
@@ -0,0 +1,49 @@
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+import sys
+import os
+
+cppInit = """#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+  return 0;
+}
+"""
+
+
+if __name__ == '__main__':
+  url = 'https://open.kattis.com/search?q='+sys.argv[1]
+  response = requests.get(url)
+  if "search" in response.url:
+    print("please write the exact problem id")
+    sys.exit()
+  problemId = response.url.split('/')[-1]
+  problemDir = './'+problemId
+  problemPath = problemDir+"/"+problemId+".cpp"
+  if not os.path.exists(problemDir):
+    os.mkdir(problemDir)
+  if not os.path.exists(problemPath):
+    with open(problemPath, 'w') as f:
+      f.write(cppInit)
+  
+  soup = BeautifulSoup(response.text, "html.parser")
+  tcs = soup.find_all('pre')
+  tcs = [s.text.replace('<pre>','') for s in tcs]
+  tcs = [s.replace('</pre>','') for s in tcs]
+  tccount = 1
+
+  makeInit = "compile:\n\tg++ -std=c++17 -Wall -static -O2 {0} -o run.out\ntest:\n\tg++ -std=c++17 -Wall -static -O2 {0} -o run.out".format(problemId+".cpp")
+  for k in range(0,len(tcs),2):
+    with open(problemDir+'/in'+str(tccount)+".txt", 'w') as f:
+      f.write(tcs[k])
+    with open(problemDir+'/out'+str(tccount)+".txt", 'w') as f:
+      f.write(tcs[k+1])
+    makeInit += "\n\t./run.out < in{}.txt > temp".format(tccount)
+    makeInit += "\n\tdiff out{}.txt temp".format(tccount)
+    tccount += 1
+  with open(problemDir+'/makefile', 'w') as f:
+    f.write(makeInit)
